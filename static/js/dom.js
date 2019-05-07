ed7e6fbfc3605a7aef40634bf9b3dfea1daf97fe
Tamas Floch <tamasfloch95@gmail.com>
2019-05-07 16:53:03 +0200

    Board added from csv file to html.

--- static/js/dom.js
+++ static/js/dom.js
@@ -34,16 +34,21 @@ export let dom = {
 
         for(let board of boards){
             boardList += `
-                <li>${board.title}</li>
+                <section class="board">
+                    <div class="board-header"><span class="board-title">${board['title']}</span>
+                        <button class="board-add">Add Card</button>
+                        <button class="board-toggle"><i class="fas fa-chevron-down"></i></button>
+                    </div>
+                    
+                </section>
             `;
         }
 
         const outerHtml = `
-            <ul class="board-container">
+            <div class="board-container">
                 ${boardList}
-            </ul>
+            </div>
         `;
-
         this._appendToElement(document.querySelector('#boards'), outerHtml);
     },
     loadCards: function (boardId) {
